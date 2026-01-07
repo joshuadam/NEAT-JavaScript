8d83efe3ec826cac4ef984ec0c96affc6913ec0d
Cedric <mail@cedricgasser.com>
2026-01-07 21:06:48 +0100

    Added JSDoc comments to activation function classes

--- src/activationfunction/Gaussian.js
+++ src/activationfunction/Gaussian.js
@@ -1,12 +1,33 @@
 const ActivationFunction = require('./ActivationFunction');
 
+/**
+ * Bell-shaped function that outputs its maximum value at zero and
+ * decreases as input moves away from zero in either direction.
+ * Useful for radial basis function networks.
+ * 
+ * Formula: `G(x) = e^(-x²)`
+ * 
+ * @extends ActivationFunction
+ */
 class Gaussian extends ActivationFunction {
+  /**
+   * Creates a new Gaussian activation function.
+   * @param {number} center - The center point of the Gaussian curve.
+   * @param {number} width - The width (standard deviation) of the Gaussian curve.
+   */
   constructor(center = 0, width = 1) {
     super();
+    /** @type {number} */
     this.center = center;
+    /** @type {number} */
     this.width = width;
   }
-  
+
+  /**
+   * Applies the Gaussian activation function to a value.
+   * @param {number} value - The input value.
+   * @returns {number} The Gaussian activation output in range (0, 1].
+   */
   apply(value) {
     return Math.exp(-Math.pow((value - this.center) / this.width, 2));
   }
