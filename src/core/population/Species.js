f061028ab3b2f53eb000ac5c682944b12833055c
Cedric <mail@cedricgasser.com>
2026-01-15 09:05:49 +0100

    Added JSDoc comments to genome and population classes

--- src/core/population/Species.js
+++ src/core/population/Species.js
@@ -1,15 +1,39 @@
+/** @typedef {import('../../config/Config')} Config */
+/** @typedef {import('../genome/Genome')} Genome */
+
+/**
+ * Represents a species in the NEAT algorithm.
+ */
 class Species {
+  /**
+   * Creates a new Species instance.
+   * @param {number} id - The unique identifier for this species.
+   * @param {Config} config - The configuration object.
+   */
   constructor(id, config) {
+    /** @type {number} */
     this.id = id;
+    /** @type {Config} */
     this.config = config;
+    /** @type {number} */
     this.bestFitness = 0;
+    /** @type {number} */
     this.generationsSinceImprovement = 0;
+    /** @type {boolean} */
     this.stagnated = false;
+    /** @type {Genome[]} */
     this.genomes = [];
+    /** @type {Genome|null} */
     this.representative = null;
+    /** @type {number} */
     this.offspringCount = 0;
   }
 
+  /**
+   * Adds a genome to this species.
+   * If this is the first genome, it becomes the representative.
+   * @param {Genome} genome - The genome to add.
+   */
   addGenome(genome) {
     if (this.genomes.length === 0) {
       this.representative = genome;
@@ -17,12 +41,19 @@ class Species {
     this.genomes.push(genome);
   }
 
+  /**
+   * Calculates and sets the adjusted fitness for all genomes in this species.
+   * Adjusted fitness is the raw fitness divided by the species size (fitness sharing).
+   */
   setAdjustedFitness() {
     for (const genome of this.genomes) {
       genome.adjustedFitness = genome.fitness / this.genomes.length;
     }
   }
 
+  /**
+   * Removes the worst-performing genomes from this species.
+   */
   removeBadGenomes() {
     this.genomes.sort((a, b) => b.fitness - a.fitness);
 
@@ -34,6 +65,9 @@ class Species {
     }
   }
 
+  /**
+   * Updates the species' best fitness and stagnation tracking.
+   */
   updateFitnessAndStagnation() {
     let currentBest = this.getBestGenome().fitness;
     if (currentBest > this.bestFitness) {
@@ -47,6 +81,10 @@ class Species {
     }
   }
 
+  /**
+   * Calculates the total adjusted fitness of all genomes in this species.
+   * @returns {number} The sum of all adjusted fitness values.
+   */
   getTotalAdjustedFitness() {
     let total = 0;
     for (const genome of this.genomes) {
@@ -55,6 +93,10 @@ class Species {
     return total;
   }
 
+  /**
+   * Gets the genome with the highest fitness in this species.
+   * @returns {Genome} The best-performing genome.
+   */
   getBestGenome() {
     let bestGenome = this.genomes[0];
     for (const genome of this.genomes) {
@@ -65,6 +107,9 @@ class Species {
     return bestGenome;
   }
 
+  /**
+   * Sets a random genome from this species as the new representative.
+   */
   setRandomRepresentative() {
     if (this.genomes.length === 0) {
       this.representative = null;
