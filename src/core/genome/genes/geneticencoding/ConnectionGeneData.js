c852f1ecae7ea2fb14e1170ba08dbe14cc146469
Cedric <mail@cedricgasser.com>
2026-01-15 09:05:08 +0100

    Added JSDoc comments to gene classes

--- src/core/genome/genes/geneticencoding/ConnectionGeneData.js
+++ src/core/genome/genes/geneticencoding/ConnectionGeneData.js
@@ -1,10 +1,28 @@
+/**
+ * Object representing a connection gene's properties.
+ */
 class ConnectionGeneData {
+  /**
+   * Creates a new ConnectionGeneData instance.
+   * @param {number} inNodeId - The ID of the input (source) node.
+   * @param {number} outNodeId - The ID of the output (target) node.
+   * @param {number} weight - The connection weight.
+   * @param {boolean} enabled - Whether the connection is enabled.
+   * @param {number} innovationNumber - The innovation number of this connection.
+   * @param {boolean} recurrent - Whether this is a recurrent connection.
+   */
   constructor(inNodeId, outNodeId, weight, enabled, innovationNumber, recurrent) {
+    /** @type {number} */
     this.inNodeId = inNodeId;
+    /** @type {number} */
     this.outNodeId = outNodeId;
+    /** @type {number} */
     this.weight = weight;
+    /** @type {boolean} */
     this.enabled = enabled;
+    /** @type {number} */
     this.innovationNumber = innovationNumber;
+    /** @type {boolean} */
     this.recurrent = recurrent;
   }
 }
--- src/core/genome/genes/geneticencoding/NodeGeneData.js
+++ src/core/genome/genes/geneticencoding/NodeGeneData.js
@@ -1,6 +1,16 @@
+/**
+ * Lightweight data class representing node gene information
+ */
 class NodeGeneData {
+  /**
+   * Creates a new NodeGeneData instance.
+   * @param {number} id - The unique identifier
+   * @param {'INPUT' | 'OUTPUT' | 'HIDDEN' | 'BIAS'} nodeType - The type of node ('INPUT', 'OUTPUT', 'HIDDEN', or 'BIAS').
+   */
   constructor(id, nodeType) {
+    /** @type {number} */
     this.id = id;
+    /** @type {'INPUT' | 'OUTPUT' | 'HIDDEN' | 'BIAS'} */
     this.nodeType = nodeType;
   }
 }
